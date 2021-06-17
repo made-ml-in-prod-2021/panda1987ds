e29a392ea59eda09a68162aaf31977bdcc0d7b1d
Dasha <pankratova.dasha@gmail.com>
2021-06-17 16:39:53 +0300

    Revert "hw4 v1"
    This reverts commit 818c37d5

--- online_inference/src/app.py
+++ online_inference/src/app.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi.exceptions import RequestValidationError
 import uvicorn
-import time
 
 from entities.model import Model
 from entities.request_data_item import RequestDataItem
@@ -20,18 +19,12 @@ logger = logging.getLogger(__name__)
 model = None
 MODEL_PATH = "../models/model.pkl"
 PORT = 8080
-BREAK_TIME = 120
-SLEEP_TIME = 30
 IP = '0.0.0.0'
 
 
 @app.on_event("startup")
 def load_model():
     global model
-    logger.info('Start sleep')
-    time.sleep(SLEEP_TIME)
-    global start_time
-    start_time = time.time()
     logger.info('Start load model')
     try:
         model = Model(MODEL_PATH)
@@ -43,10 +36,6 @@ def load_model():
 
 @app.get('/touch')
 def touch() -> bool:
-    global model
-    if time.time() - start_time > BREAK_TIME:
-        logger.error(f'Time is over')
-        model = None
     return model is not None and model.ready_to_use
 
 
@@ -57,36 +46,27 @@ async def validation_exception_handler(request, exc):
 
 @app.get('/')
 def main():
-    if touch():
-        return 'Please use the command "predict"'
-    else:
-        raise HTTPException(status_code=500, detail="Not ready to use")
+    return 'Please use the command "predict"'
 
 
 @app.post('/predict/', response_model=List[Response])
 def predict(request: List[RequestDataItem]) -> List[Response]:
-    if touch():
-        try:
-            logger.info('Start request...')
-            predicts = model.predict(request)
-            logger.info('Request completed successfully')
+    try:
+        logger.info('Start request...')
+        predicts = model.predict(request)
+        logger.info('Request completed successfully')
 
-            return predicts
+        return predicts
 
-        except Exception as ex:
-            logger.error(f'Critical model error: {str(ex)}')
-            raise HTTPException(status_code=500,
-                                detail="Critical error. Can't make a prediction")
-    else:
-        logger.error(f'Not ready to use')
+    except Exception as ex:
+        logger.error(f'Critical model error: {str(ex)}')
         raise HTTPException(status_code=500,
-                                detail="Critical error. Can't make a prediction. Not ready to use")
+                            detail="Critical error. Can't make a prediction")
 
 
 def main():
     load_model()
     uvicorn.run(app, host=IP, port=PORT)
-
 
 if __name__ == "__main__":
     main()
